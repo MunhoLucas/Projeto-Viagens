a03c7a8a3c05b5249362897586893896bbac2348
LucasMunho <lucasmunho@xds.com.br>
2021-09-27 10:00:28 -0300

    testando

--- TesteLucasGit/TesteLucasGit/ViewController.swift
+++ TesteLucasGit/TesteLucasGit/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    var Pessoa: String = "Lucas"
+    let Pessoa: String = "Lucas"
+    
 
 }
 

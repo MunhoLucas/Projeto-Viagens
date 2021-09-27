4461acd5fe47d9ab3db6dfd0f1b4fb3223aed840
LucasMunho <lucasmunho@xds.com.br>
2021-09-27 11:50:53 -0300

    adicionando metodo Delegate da TableView para o Header aparecer

---
+++ TesteLucasGit/TesteLucasGit/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  TesteLucasGit
+//
+//  Created by Lucas Munho on 27/09/21.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

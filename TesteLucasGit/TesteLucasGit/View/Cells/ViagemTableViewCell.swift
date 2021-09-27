02e160d59c4dd952bfb549312f0e2ddd50f10b1f
LucasMunho <lucasmunho@xds.com.br>
2021-09-27 14:48:59 -0300

    Criacao de xib para implementacao de celula customizada

---
+++ TesteLucasGit/TesteLucasGit/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  TesteLucasGit
+//
+//  Created by Lucas Munho on 27/09/21.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

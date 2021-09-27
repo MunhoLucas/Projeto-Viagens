c3fcb54354ac35689d42759c619b4acd65c9e728
LucasMunho <lucasmunho@xds.com.br>
2021-09-27 12:15:00 -0300

    Alteracoes no layoute do projeto para comecar a mexer nas constraints

--- TesteLucasGit/TesteLucasGit/HomeTableViewHeader.swift
+++ TesteLucasGit/TesteLucasGit/HomeTableViewHeader.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class HomeTableViewHeader: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    
+    @IBOutlet weak var tituloLabel: UILabel!
+    
+    @IBOutlet weak var headerView: UIView!
+    
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
+    func configuraView(){
+        headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+        
+        
     }
-    */
-
+    
 }
